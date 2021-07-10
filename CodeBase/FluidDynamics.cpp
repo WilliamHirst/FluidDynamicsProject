2d07c87c0bb680df96cce7c9227c9b1769ff08d1
William Hirst <bod_holthe@outlook.com>
2021-07-10 19:22:37 +0200

    Created 9 matrices.

--- CodeBase/FluidDynamics.cpp
+++ CodeBase/FluidDynamics.cpp
@@ -11,6 +11,8 @@ void FluidDynamics::Initialize(double width, double height, double omega)
 {
   const double boltzmann_constant = 1.3806503e-23;
   const double nu = 1./3. * (1./omega - 0.5);
+  double **nZero = nOne = nTwo = nThree = nFoure = nFive = nSix = nSeven = nEight = nNine = createMatrix(width, height);
+
 }
 
 double ** FluidDynamics::createMatrix(double width, double height) {
@@ -20,5 +22,11 @@ double ** FluidDynamics::createMatrix(double width, double height) {
   // Allocate memory
   for(int i=0;i<height;i++)
       matrix[i] = new double[width];
-
+  // Set values to zero
+  for(int i = 0; i < height; i++){
+      for(int j = 0; j < width; j++){
+          matrix[i][j] = 0.0;
+      }
+  }
+  return matrix;
 }
